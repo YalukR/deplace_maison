650b0736c7f6b78cd0a5338d10d298f30f144102
YalukR <yalukramos@gmail.com>
2026-06-06 01:05:42 -0600

    scroll

--- lib/layout/app-bar.dart
+++ lib/layout/app-bar.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:deplace_maison/layout/widgets/way_archive.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
-  const AppBarWidget({super.key});
+  final bool showInkBar;
+  const AppBarWidget({super.key, this.showInkBar = true});
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 80);
+  Size get preferredSize =>
+      Size.fromHeight(showInkBar ? kToolbarHeight + 80 : 80);
 
   @override
   Widget build(BuildContext context) {
@@ -14,33 +16,45 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         WayArchive(),
-        Row(
-          children: [
-            const Expanded(child: SizedBox()),
-
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(color: Colors.black, width: 1),
-              ),
-              child: const Text(
-                'INK MODE',
-                style: TextStyle(
-                  fontFamily: 'FingerPaint',
-                  fontSize: 13,
-                  color: Colors.black,
-                ),
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 250),
+          curve: Curves.easeInOut,
+          height: showInkBar ? kToolbarHeight : 0,
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: SizedBox(
+              height: kToolbarHeight,
+              child: Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.black, width: 1),
+                    ),
+                    child: const Text(
+                      'INK MODE',
+                      style: TextStyle(
+                        fontFamily: 'FingerPaint',
+                        fontSize: 13,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: TextWidget(text: 'CART'),
+                    ),
+                  ),
+                ],
               ),
             ),
-
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: TextWidget(text: 'CART'),
-              ),
-            ),
-          ],
+          ),
         ),
       ],
     );
