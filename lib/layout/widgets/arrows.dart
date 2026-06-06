87d94954d4e4af54f5e7b90f14a3e948943e9279
YalukR <yalukramos@gmail.com>
2026-06-06 15:22:48 -0600

    hover

--- lib/layout/widgets/arrows.dart
+++ lib/layout/widgets/arrows.dart
@@ -22,6 +22,7 @@ class _ArrowsWidgetState extends State<ArrowsWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _rotation;
+  late Animation<double> _scale;
 
   @override
   void initState() {
@@ -33,6 +34,11 @@ class _ArrowsWidgetState extends State<ArrowsWidget>
     _rotation = Tween<double>(
       begin: 0,
       end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    _scale = Tween<double>(
+      begin: 1.0,
+      end: 1.4,
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
   }
 
@@ -76,11 +82,14 @@ class _ArrowsWidgetState extends State<ArrowsWidget>
             const SizedBox(width: 16),
           ],
           AnimatedBuilder(
-            animation: _rotation,
+            animation: _controller,
             builder: (context, child) {
-              return Transform.rotate(
-                angle: (_baseAngle + _rotation.value) * 2 * 3.14159,
-                child: child,
+              return Transform.scale(
+                scale: _scale.value,
+                child: Transform.rotate(
+                  angle: (_baseAngle + _rotation.value) * 2 * 3.14159,
+                  child: child,
+                ),
               );
             },
             child: Container(
